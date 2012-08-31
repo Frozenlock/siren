f7f80d2ba7183f0bd9490fa14a8d1bf69428a6cd
Frozenlock <frozenlock@gmail.com>
2012-08-31 13:23:03 -0400

    correct core.clj for core.cljs. Silly me.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject siren "0.1.0"
+(defproject siren "0.1.1"
   :description "Easy notifications, Growl style (Clojurescript)"
   :source-path "src"
   :license {:name "Eclipse Public License"
